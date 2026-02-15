27b0fdfb94edee76e11f4896b950fc6a22465a82
Zahra148 <nayyabzahra148@gmail.com>
2026-02-15 23:08:02 +0500

    Delete Task 4 context_aware_rag_chatbot directory

--- Task 4 context_aware_rag_chatbot/rag/memory.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.chat_history import InMemoryChatMessageHistory
-
-
-def get_memory():
-    """
-    Returns in-memory chat history object (LangChain v1 compatible)
-    """
-    return InMemoryChatMessageHistory()
